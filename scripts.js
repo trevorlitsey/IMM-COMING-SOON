ffedb1aff6a1ad460cb11a8dc576921575a3e95a
Trevor Litsey <trevorlitsey@gmail.com>
2018-11-12 21:46:58 -0600

    eric edits

--- scripts.js
+++
@@ -1,8 +0,0 @@
-const homeButton = document.querySelector('.home-button');
-const homeList = document.querySelector('.home-list');
-
-function toggleHome() {
-	homeList.classList.toggle('on');
-}
-
-homeButton.addEventListener('click', toggleHome);
